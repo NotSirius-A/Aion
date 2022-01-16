be2e816c5149f2a92ba01020e997b467ea1c423f
NotSirius-A <filip.grzymski@gmail.com>
2022-01-16 13:53:45 +0100

    im working solo no one reads these anyway

--- Aion/Aion.ino
+++ Aion/Aion.ino
@@ -1,16 +1,20 @@
 #include "config.h"
 
-unsigned long timeNow_ms = 0;
-unsigned long timeLast_ms = 0;
-uint16_t updatePeriod_ms = 30;
+unsigned long timeNow  = 0;
+unsigned long timeLast = 0;
+uint16_t updatePeriod = 30;
+
+unsigned long debounceTimeLast = 0;
+uint8_t debouncePeriod = 50;
 
 uint8_t currentState = 0;
 uint32_t stateLengths[NUM_OF_STATES];
-int32_t lastTransitionTime_ms = 0;
+int32_t lastTransitionTime = 0;
 
 uint8_t currentMode = 0;
 
-uint16_t prevBtn = 1;
+uint16_t modeButtonState = 0;
+uint16_t prevModeButtonState = 0;
 
 void setup() {
 
@@ -25,26 +29,28 @@ void setup() {
 }
 
 void loop() {
-  timeNow_ms = millis();
-  delay(10);
-  uint16_t buttonState = digitalRead(ENCODER_BTN);
-  if (buttonState == LOW && prevBtn == HIGH) {
-    currentMode++;
-    if (currentMode >= NUM_OF_MODES){
-      currentMode = 0;
+  timeNow = millis();
+
+  if ((timeNow - debounceTimeLast) > debouncePeriod) {
+    debounceTimeLast = timeNow;
+    modeButtonState = digitalRead(MODE_BTN);
+    if (modeButtonState == LOW && prevModeButtonState == HIGH) {
+      currentMode++;
+      if (currentMode >= NUM_OF_MODES) {
+        currentMode = 0;
+      }
     }
-   }
-  prevBtn = buttonState;
-  
+    prevModeButtonState = modeButtonState;
+  }
   
 
-  if ((timeNow_ms - timeLast_ms) > updatePeriod_ms){
-    timeLast_ms = timeNow_ms;
+  if ((timeNow - timeLast) > updatePeriod){
+    timeLast = timeNow;
 //    Serial.println(getEncoderValue());
 
-    Serial.println(currentMode);
+//    Serial.println(currentMode);
 
-    int x = getTimeUntilTransition(timeNow_ms, lastTransitionTime_ms, currentState, stateLengths);
+    int x = getTimeUntilTransition(timeNow, lastTransitionTime , currentState, stateLengths);
     x = x / 1000;
     uint8_t hours = x / 3600;
     uint8_t minutes = (x / 60) - (hours * 60);
@@ -58,7 +64,7 @@ void loop() {
   
     if (x < 0) {
       Serial.println("======================");
-      lastTransitionTime_ms = timeNow_ms;
+      lastTransitionTime = timeNow ;
       currentState++;
       if (currentState >= NUM_OF_STATES) {
         currentState = 0; 
