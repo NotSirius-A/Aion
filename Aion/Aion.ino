e85ea4042efc869ed787b2a4c621d80594d988cb
NotSirius-A <filip.grzymski@gmail.com>
2022-01-21 20:42:37 +0100

    dsf

--- Aion/Aion.ino
+++ Aion/Aion.ino
@@ -1,8 +1,5 @@
 #include "config.h"
 
-unsigned long timeLast = 0;
-const uint16_t updatePeriod = 30;
-
 unsigned long debounceTimeLast = 0;
 
 uint8_t currentState = 0;
@@ -14,45 +11,53 @@ uint16_t prevModeButtonState = 0;
 
 int16_t lastEncoderValue = 0;
 
-uint32_t stateLengths[] = STATE_LENGTHS;
-size_t numOfStates = 3;
-void setup() {
+uint8_t dutyCycleLCDPercent = DUTY_CYCLE_LCD_PERCENT_DEFAULT;
+bool isStealthMode = STEALTHMODE_DEFAULT;
 
+uint32_t stateLengths[] = STATE_LENGTHS;
+size_t numOfStates = sizeof(stateLengths)/sizeof(*stateLengths);
+
+uint8_t volume = VOLUME_DEFAULT;
+
+void setup() {
   Serial.begin(SERIAL_BAUD_RATE);
-  
-  initializeEncoder(ENCODER_CLK, ENCODER_DT, ENCODER_BTN);
-  lastEncoderValue = getEncoderValue();
-  
+  pinMode(MODE_BTN, INPUT_PULLUP);
+  pinMode(5, OUTPUT);
+
+
+  initializeEncoder(ENCODER_CLK, ENCODER_DT, ENCODER_BTN);  
 }
 
 void loop() {
-
   unsigned long timeNow = millis();
 
-  int8_t encoderValueChange;
-  int16_t encoderValue;  
+  int16_t encoderValue = getEncoderValue();
+  int8_t encoderValueChange = encoderValue - lastEncoderValue;
+  lastEncoderValue = encoderValue;
+  delay(10);
+
 
   if ((timeNow - debounceTimeLast) > DEBOUNCE_PERIOD) {
     uint16_t modeButtonState = digitalRead(MODE_BTN);
     debounceTimeLast = timeNow;
-    prevModeButtonState = modeButtonState;
-    
+
     if (modeButtonState == LOW && prevModeButtonState == HIGH) {
       currentMode++;
       if (currentMode >= NUM_OF_MODES) {
         currentMode = 0;
       }
     }
+    prevModeButtonState = modeButtonState;
   }
-  
-  
+
   switch(currentMode) {
-    case 0:  
-      int32_t timeUntilTransition = getTimeUntilTransition(timeNow, lastTransitionTime , currentState, stateLengths);
+    case 0:
+      int32_t timeUntilTransition;   
+      timeUntilTransition = getTimeUntilTransition(timeNow, lastTransitionTime , currentState, stateLengths);
       uint8_t hours, minutes, seconds;
       convertMillisToHumanFormat(timeUntilTransition, hours, minutes, seconds);
 
-      printTimeToLCD(hours, minutes, seconds);
+      //printTimeToLCD(hours, minutes, seconds);
       
       if (timeUntilTransition <= 0) {
         currentState++;
@@ -65,6 +70,7 @@ void loop() {
     
         // Execute some code when transition to another state happens, for example let user know a transition happened with sound etc.
         for (bool i = false; i==false;) {
+          Serial.println("---------");
           i = handleTransition();
         }
       }
@@ -74,9 +80,20 @@ void loop() {
       break;
     case 2:
       //handleDutyCycleEditMode(encoderValueChange);
+//        dutyCycleLCDPercent += encoderValueChange * 5;
+//  dutyCycleLCDPercent = constrain(dutyCycle, 0, 100);
       break;
     case 3:
       //handleStealthinessEditMode(encoderValueChange);
+//              if (encoderValueChange > 0) {
+//          isStealthMode = true;
+//        } else if (encoderValueChange < 0) {
+//          isStealthMode = false;
+//        }
+      break;
+    case 4:
+      handleVolumeControlMode(volume, isStealthMode, encoderValueChange);
+      analogWrite(5, volume);
       break;
     default:
       Serial.println("def");
--- Aion/modes.ino
+++ Aion/modes.ino
@@ -1,3 +1,34 @@
 void handleStateLengthEditMode(int8_t encoderValueChange) {
   Serial.println("handleStateLengthEditMode Not possible to implement with current code flow");
 }
+
+void handleVolumeControlMode(uint8_t & volume, bool isStealth, int8_t change) {
+  /* This function takes in volume and change in argument and returns new volume
+   Firstly, given volume is used to get the original function argument, then
+   change in argument is added to the calculated value, so that the new value can be
+   plugged into the volume function to get the updated volume
+  */ 
+
+  /* There is only natural log natively in Arduino code, here other log base is required, fortunately
+    there's a formula that can be used here: logx(z) = logy(z) / logy(x)
+  */
+
+  double a = VOLUME_CONTROL_EXPONENT_COEFFICIENT;
+  double x = (log(volume+1)/log(2))/a;
+
+  double exponent = double(x+change)*a;
+  double result = pow(2.0, exponent)-1;
+
+  /* 
+    PWM can only support int 0-255, so `result` has to be rounded up
+  */ 
+
+  if (change >= 0) {
+    result = round(result+0.45);
+  } else if (change < 0) {
+    result = round(result-0.45);
+  } 
+
+  // 'Return' volume
+  volume = constrain(trunc(result), 0, 255);
+}
